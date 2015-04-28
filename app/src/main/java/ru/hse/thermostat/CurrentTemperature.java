70e38cadc3f846236fb49c09250d219b4ac4b7ef
Aleksander <litunone@gmail.com>
2015-04-28 19:41:40 +0300

    Card shadow

--- app/src/main/java/ru/hse/thermostat/CurrentTemperature.java
+++ app/src/main/java/ru/hse/thermostat/CurrentTemperature.java
@@ -19,29 +19,12 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class CurrentTemperature extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
     CardView mCardView;
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment CurrentTemperature.
-     */
-    // TODO: Rename and change types and number of parameters
     public static CurrentTemperature newInstance() {
         CurrentTemperature fragment = new CurrentTemperature();
         fragment.setRetainInstance(true);
-//        Bundle args = new Bundle();
-////        args.putString(ARG_PARAM1, param1);
-////        args.putString(ARG_PARAM2, param2);
-//        fragment.setArguments(args);
         return fragment;
     }
 
@@ -75,5 +58,7 @@ public class CurrentTemperature extends Fragment {
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         mCardView = (CardView) view.findViewById(R.id.cardview);
+//        mCardView.setRadius(1f);
+//        mCardView.setCardElevation(10);
     }
 }
