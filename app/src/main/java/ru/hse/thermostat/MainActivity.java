cb0b204bf717cdb1bd4c2b440e58a3b145297e07
Aleksander <litunone@gmail.com>
2015-05-02 04:00:10 +0300

    Empty menu

--- app/src/main/java/ru/hse/thermostat/MainActivity.java
+++ app/src/main/java/ru/hse/thermostat/MainActivity.java
@@ -15,9 +15,9 @@ public class MainActivity extends Activity {
         if (savedInstanceState == null) {
             getFragmentManager().beginTransaction()
                     .add(R.id.current_temperature_container, CurrentTemperature.newInstance())
-                    .add(R.id.next_temperature_container, CurrentTemperature.newInstance())
                     .commit();
         }
+        
     }
 
 
